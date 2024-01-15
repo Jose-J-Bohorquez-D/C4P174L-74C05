b1c73f4eb746a031a62828c48a2fff02c5226b1d
Alejandra Sanchez <alejitasanvar@gmail.com>
2024-01-15 02:31:00 -0500

    ajustamos el scrollSpy, insertamos un carrusel y mantenemos el tema

--- Vistas/Landing/Paginas/inicio.php
+++ Vistas/Landing/Paginas/inicio.php
@@ -1,11 +1,86 @@
-<div class="container-fluid">
-	<div class="row">
-		<div class="col">
-			<br><br>
-			<h1 class="text-center">Marcas</h1>
-			<div class="container-fluid">
-				
-			</div>
-		</div>
-	</div>
+
+<br><br>
+<div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example p-3 rounded-2" tabindex="0">
+  <h4 id="scrollspyHeading1">
+    <h6 class="text-center">Inicio</h6>
+  </h4>
+  
+<div id="carouselExampleDark" class="col-sm-12 col-md-8 col-lg-6 text-center carousel carousel-dark slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="100">
+      <img src="Assets/Imgs/adam.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="100">
+      <img src="Assets/Imgs/predator.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+  
+  <h4 id="scrollspyHeading2"></h4>
+  <h6 class="text-center">servicios</h6>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+
+  <h4 id="scrollspyHeading3"></h4>
+  <h6 class="text-center">productos</h6>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+
+  <h4 id="scrollspyHeading4"></h4>
+  <h6 class="text-center">blog </h6>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+
+  <h4 id="scrollspyHeading5"></h4>
+  <h6 class="text-center">Contacto</h6>
+    <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
+  <br><br><br><br><br>
 </div>
